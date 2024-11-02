da38502957fbf5288b04f50068adb4df100d21f5
Adithep Sudcharee <adithep.s@rznet.com>
2024-11-02 18:59:36 +0700

    update

--- server.js
+++ server.js
@@ -12,28 +12,26 @@ const PORT = process.env.PORT || 4000;
 
 const io = socketIo(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN || '*',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
   },
 });
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || '*',
+  origin: '*',
   credentials: true,
 }));
 
 app.get('/', (req, res) => {
-  // res.send('Socket.IO server is running');
-  res.send(`Host: ${req.headers.host} Port: ${PORT}`);
+  res.send(`Host: ${req.headers.host} Port:${PORT}`);
 });
 
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  socket.on('message', (msg) => {
-    console.log('Message received:', msg);
-    io.emit('message', msg);
+  socket.on('submitPatientData', (data) => {
+    io.emit('patientFormUpdate', data);
   });
 
   socket.on('disconnect', () => {
@@ -42,6 +40,6 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on render port ${PORT}`);
-  console.log()
+  console.log(`Socket.IO server running`);
 });
+
